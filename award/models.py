aa195b8a51cd192fe4aa6484e697078defa039be
murathe <isaacsm07p@gmail.com>
2020-02-17 11:34:12 +0300

    Correct uploadcare

--- award/models.py
+++ award/models.py
@@ -10,17 +10,17 @@ from django.core.validators import MaxValueValidator
 # Create your models here.
 class Profile(models.Model):
     bio = HTMLField()
-    profile_photo = ImageField(blank = True)
+    profile_photo = ImageField()
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     phone_number = models.CharField(max_length=12)
 
     @receiver(post_save, sender = User)
-    def create_user_profile(sender, instance, created, **kwargs):
+    def create_user_profile(self, sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
 
     @receiver(post_save, sender = User)
-    def save_user_profile(sender, instance, **kwargs):
+    def save_user_profile(self, sender, instance, **kwargs):
         instance.profile.save()
 
     def save_profile(self):
