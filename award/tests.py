8f30d834803e61e73b949cc92984abfd3cc8d864
murathe <isaacsm07p@gmail.com>
2020-02-20 11:02:56 +0300

    Ready to deploy to heroku

--- award/tests.py
+++ award/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Profile,Projects,Rates
 
-# Create your tests here.
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.murathe = Profile(profile_photo='def.jpg',bio='Murathe is a gem', phone_number='0722100100')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.murathe,Profile))
+
+    def test_save(self):
+        self.murathe.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles)>0)
+ 
